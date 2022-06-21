d480800df05de33d31687afb7fff29c5a20f6bb2
madisonlowe <mcourteney@live.com>
2022-06-21 16:09:38 +0100

    Fixed map function.

--- front-end-room-21/src/components/App/App.js
+++ front-end-room-21/src/components/App/App.js
@@ -2,18 +2,28 @@
 import "./App.css";
 import Biography from "../Biography/biography";
 import Navbar from "../Navbar/navbar.js";
-
 import Dropdown from "../Dropdown/dropdown";
-
 import Searchbar from "../Searchbar/Searchbar.js";
 
+const bootcampers = [
+  { name: "Jim Halpert", id: 1 },
+  { name: "Pam Beasley", id: 2 },
+  { name: "Dwight Schrute", id: 3 },
+  { name: "Michael Scott", id: 4 },
+  { name: "Ron Swanson", id: 5 },
+];
 
 function App() {
   return (
     <main className="App">
       <Navbar />
-      <Dropdown/>
+      <Dropdown />
       <Searchbar />
+      <div>
+        {bootcampers.map((item) => (
+          <Biography id={item.id} bcName={item.name} />
+        ))}
+      </div>
       <Biography />
     </main>
   );
--- front-end-room-21/src/components/Biography/biography.js
+++ front-end-room-21/src/components/Biography/biography.js
@@ -1,19 +1,32 @@
-import "./biography.css"
+import "./biography.css";
 // import {useState} from "react"
 
-
-function Biography (){
-    return (
-        <div className ='main-container'>
-        <div className= "div-Container">
-     <img className="bootcamper-picture"src = 'https://static.vecteezy.com/system/resources/previews/002/002/247/non_2x/beautiful-black-woman-avatar-character-icon-free-vector.jpg' alt='bio for bootcampers'/>
-        <p className= 'name'>Sandy Beech</p>
-        <p className= 'jobTitle'><em> Current Bootcamper</em></p>
-        <p className='region'><b>Birmingham</b></p>
-        <p className= 'bio'>"Looking for side-projects partners or Team! more info in bio!" </p>
-        <a className = 'link'href="https://www.google.com/">click more</a>
-        </div>
-     </div>
-    )
+function Biography({ bcName, id }) {
+  return (
+    <div className="main-container">
+      <div className="div-Container">
+        <img
+          className="bootcamper-picture"
+          src="https://static.vecteezy.com/system/resources/previews/002/002/247/non_2x/beautiful-black-woman-avatar-character-icon-free-vector.jpg"
+          alt="bio for bootcampers"
+        />
+        <p className="name" key={id}>
+          {bcName}
+        </p>
+        <p className="jobTitle">
+          <em> Current Bootcamper</em>
+        </p>
+        <p className="region">
+          <b>Birmingham</b>
+        </p>
+        <p className="bio">
+          "Looking for side-projects partners or Team! more info in bio!"{" "}
+        </p>
+        <a className="link" href="https://www.google.com/">
+          click more
+        </a>
+      </div>
+    </div>
+  );
 }
-export default Biography
+export default Biography;
