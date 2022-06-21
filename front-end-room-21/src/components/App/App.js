ce782f5450edb8df82b2b71970abdca76d7fbe15
nafisoaden97 <103136799+Udecunup5@users.noreply.github.com>
2022-06-21 15:11:26 +0100

    drop down completed

--- front-end-room-21/src/components/App/App.js
+++ front-end-room-21/src/components/App/App.js
@@ -2,11 +2,13 @@
 import "./App.css";
 import Biography from "../Biography/biography";
 import Navbar from "../Navbar/navbar.js";
+import Dropdown from "../Dropdown/dropdown";
 
 function App() {
   return (
     <main className="App">
       <Navbar />
+      <Dropdown/>
       <Biography />
     </main>
   );
--- front-end-room-21/src/components/Biography/biography.js
+++ front-end-room-21/src/components/Biography/biography.js
@@ -8,9 +8,10 @@ function Biography (){
         <div className= "div-Container">
      <img className="bootcamper-picture"src = 'https://static.vecteezy.com/system/resources/previews/002/002/247/non_2x/beautiful-black-woman-avatar-character-icon-free-vector.jpg' alt='bio for bootcampers'/>
         <p className= 'name'>Sandy Beech</p>
-        <p className= 'profession'><b>Birmingham</b><em> Current Bootcamper</em></p>
+        <p className= 'jobTitle'><em> Current Bootcamper</em></p>
+        <p className='region'><b>Birmingham</b></p>
         <p className= 'bio'>"Looking for side-projects partners or Team! more info in bio!" </p>
-        <a href="https://www.google.com/">click more</a>
+        <a className = 'link'href="https://www.google.com/">click more</a>
         </div>
      </div>
     )
