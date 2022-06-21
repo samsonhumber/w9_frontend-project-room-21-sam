ed7f884e1ad8aabfa30c8b34fc782fadeb09709b
nafisoaden97 <103136799+Udecunup5@users.noreply.github.com>
2022-06-21 12:16:17 +0100

    template for div completed

--- front-end-room-21/src/components/Biography/biography.js
+++ front-end-room-21/src/components/Biography/biography.js
@@ -6,11 +6,11 @@ function Biography (){
     return (
         <div className ='main-container'>
         <div className= "div-Container">
-     <img className="bootcamper-picture"src = 'https://www.singleton-associates.org/wp-content/uploads/2010/08/Free.jpg' alt='bio for bootcampers'/>
-        <p className= 'name'>name</p>
-        <p className= 'profession'>profession</p>
-        <p className= 'bio'>bio</p>
-        <a href="https://www.google.com/">link</a>
+     <img className="bootcamper-picture"src = 'https://static.vecteezy.com/system/resources/previews/002/002/247/non_2x/beautiful-black-woman-avatar-character-icon-free-vector.jpg' alt='bio for bootcampers'/>
+        <p className= 'name'>Sandy Beech</p>
+        <p className= 'profession'><b>Birmingham</b><em> Current Bootcamper</em></p>
+        <p className= 'bio'>"Looking for side-projects partners or Team! more info in bio!" </p>
+        <a href="https://www.google.com/">click more</a>
         </div>
      </div>
     )
