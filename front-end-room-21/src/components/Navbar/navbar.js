8e340d85fd92b60d6516ebce43a45490a084f96e
nafisoaden97 <103136799+Udecunup5@users.noreply.github.com>
2022-06-22 16:07:38 +0100

    aria labels added, fixed li tags in nav-bar

--- front-end-room-21/src/components/Navbar/navbar.js
+++ front-end-room-21/src/components/Navbar/navbar.js
@@ -1,30 +1,33 @@
 import "./navbar.css";
 
 function Navbar() {
-  return (
-    <nav className="nav-container">
-      <li className="nav-appname">
-        <a className="nav-link" href="google.com">
-          App Name
-        </a>
-      </li>
-      <li className="nav-point">
-        <a className="nav-link" href="google.com">
-          Home
-        </a>
-      </li>
-      <li className="nav-point">
-        <a className="nav-link" href="google.com">
-          Yearbook
-        </a>
-      </li>
-      <li className="nav-point">
-        <a className="nav-link" href="google.com">
-          Catch Up
-        </a>
-      </li>
-    </nav>
-  );
+    return (
+        <nav className="nav-container" aria-label='navbar'>
+            <ul>
+                <li className="nav-appname">
+
+                    <a className="nav-link" href="google.com">
+                        App Name
+                    </a>
+                </li>
+                <li className="nav-point">
+                    <a className="nav-link" href="google.com" aria-label='home page'>
+                        Home
+                    </a>
+                </li>
+                <li className="nav-point">
+                    <a className="nav-link" href="google.com" aria-label='year book page'>
+                        Yearbook
+                    </a>
+                </li>
+                <li className="nav-point">
+                    <a className="nav-link" href="google.com" aria-label='catchb up page'>
+                        Catch Up
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    );
 }
 
 export default Navbar;
