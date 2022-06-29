3b81573807e4a732ea81bbb0fa7fc23498255280
IsmailAli <ismailali98@gmail.com>
2022-06-29 14:28:54 +0100

    Abstracted away hooks to a different folder

--- front-end-room-21/src/components/App/App.js
+++ front-end-room-21/src/components/App/App.js
@@ -4,21 +4,15 @@ import Dropdown from "../Dropdown/dropdown";
 import Searchbar from "../Searchbar/Searchbar.js";
 import { useState, useEffect } from "react";
 import { BioList, } from "../BioList/bioList";
-import { fetchData, bioState, useRegion} from "../../hooks";
+import {useEffectFunction} from "../../hooks";
 
 function App() {
   const [text, setText] = useState();
-  const [searchtext, setSearchtext] = useState();
-  const [bioData, setBioData] = bioState();
-  //const [dropdownRegion, setDropdownRegion] = useState("");
-  //const [dropdownBootcamp, setDropdownBootcamp] = useState("");
-  //const [bioData, setBioData] = useState([]);
 
   function handleChange(e) {
     setText(e.target.value);
   }
   
-
   function handleDropdownBootcamp(e) {
     console.log("Bootcamp selected");
     setDropdownBootcamp(e.target.value);
@@ -29,8 +23,12 @@ function App() {
     setSearchtext(text);
   }
 
-  const [dropdownRegion, setDropdownRegion, fetchRegionData] = useRegion();
-  const [setDropdownBootcamp, dropdownBootcamp, fetchBootcampData] = fetchData();
+  function handleDropdownRegion(e) {
+    console.log("Region selected");
+    setDropdownRegion(e.target.value);
+  }
+
+  const [dropdownRegion, setDropdownRegion, fetchRegionData, setDropdownBootcamp, dropdownBootcamp, fetchBootcampData, bioData, setSearchtext, searchtext, fetchNameSearchData] = useEffectFunction();
 
   useEffect(() => {
      fetchRegionData();
@@ -40,74 +38,9 @@ function App() {
     fetchBootcampData();
   }, [dropdownBootcamp])
 
-
-  function handleDropdownRegion(e) {
-    console.log("Region selected");
-    setDropdownRegion(e.target.value);
-  }
-  
-
-  /*useEffect(() => {
-    async function fetchRegionData() {
-      console.log("GET request from Region dropdown.");
-      try {
-        const url = "http://localhost:9000/profiles/?region=" + dropdownRegion;
-        console.log(url);
-        const response = await fetch(url);
-        const responseJSON = await response.json();
-        const responseData = responseJSON.payload;
-        console.log(responseData);
-        setBioData(responseData);
-      } catch (err) {
-        const responseData = "Sorry, we couldn't find the data you wanted.";
-        console.log(responseData);
-      }
-    }
-    fetchRegionData();
-  }, [dropdownRegion]);
-  */
-/*
   useEffect(() => {
-    async function fetchBootcampData() {
-      console.log("GET request from Cohort dropdown.");
-      try {
-        const url =
-          "http://localhost:9000/profiles/?Bootcampnumber=" + dropdownBootcamp;
-        console.log(url);
-        const response = await fetch(url);
-        const responseJSON = await response.json();
-        const responseData = responseJSON.payload;
-        console.log(responseData);
-        setBioData(responseData);
-      } catch (err) {
-        const responseData = "Sorry, we couldn't find the data you wanted.";
-        console.log(responseData);
-      }
-    }
-    fetchBootcampData();
-  }, [dropdownBootcamp]);
-*/
-  /*useEffect(() => {
-    async function fetchNameSearch() {
-      console.log("GET request from searchbar entry.");
-      try {
-        const url = "http://localhost:9000/profiles/?name=" + searchtext;
-        console.log(url);
-        const response = await fetch(url);
-        const responseJSON = await response.json();
-        const responseData = responseJSON.payload;
-        console.log(responseData);
-        setBioData(responseData);
-      } catch (err) {
-        const responseData = "Sorry, we couldn't find the person you wanted.";
-        console.log(responseData);
-      }
-    }
-    fetchNameSearch();
-  }, [searchtext]);
-  */
-
-
+    fetchNameSearchData();
+  }, [searchtext])
 
   return (
     <main className="App">
--- front-end-room-21/src/hooks/index.js
+++ front-end-room-21/src/hooks/index.js
@@ -1,13 +1,10 @@
 import { useState } from 'react'
 
-const bioState = function BioState() {
-  const [bioData, setBioData] = useState([]);
-  return [bioData, setBioData];
-}
-
-const useRegion = function UseRegion() {
+const useEffectFunction = function UseFunction() {
     const [dropdownRegion, setDropdownRegion] = useState("");
-    const [bioData, setBioData] = bioState();
+    const [dropdownBootcamp, setDropdownBootcamp] = useState("");
+    const [bioData, setBioData] = useState([]);;
+    const [searchtext, setSearchtext] = useState();
   
     // load items
     const fetchRegionData = async () => {
@@ -25,19 +22,28 @@ const useRegion = function UseRegion() {
           console.log(responseData);
         }
     };
+
+    const fetchBootcampData = async () => {
+      console.log("GET request from Cohort dropdown.");
+        try {
+          const url =
+            "http://localhost:9000/profiles/?Bootcampnumber=" + dropdownBootcamp;
+          console.log(url);
+          const response = await fetch(url);
+          const responseJSON = await response.json();
+          const responseData = responseJSON.payload;
+          console.log(responseData);
+          setBioData(responseData);
+        } catch (err) {
+          const responseData = "Sorry, we couldn't find the data you wanted.";
+          console.log(responseData);
+        }
+    }
   
-    return [dropdownRegion, setDropdownRegion, fetchRegionData];
-};
-
-const fetchData = function FetchData() {
-  const [dropdownBootcamp, setDropdownBootcamp] = useState("");
-  const [bioData, setBioData] = bioState();
-
-  const fetchBootcampData = async () => {
-    console.log("GET request from Cohort dropdown.");
+    const fetchNameSearchData = async () => {
+      console.log("GET request from searchbar entry.");
       try {
-        const url =
-          "http://localhost:9000/profiles/?Bootcampnumber=" + dropdownBootcamp;
+        const url = "http://localhost:9000/profiles/?name=" + searchtext;
         console.log(url);
         const response = await fetch(url);
         const responseJSON = await response.json();
@@ -45,13 +51,14 @@ const fetchData = function FetchData() {
         console.log(responseData);
         setBioData(responseData);
       } catch (err) {
-        const responseData = "Sorry, we couldn't find the data you wanted.";
+        const responseData = "Sorry, we couldn't find the person you wanted.";
         console.log(responseData);
       }
-  }
-  return [setDropdownBootcamp, dropdownBootcamp, fetchBootcampData];
-}
+    }
 
+    return [dropdownRegion, setDropdownRegion, fetchRegionData, setDropdownBootcamp, dropdownBootcamp, fetchBootcampData, bioData, setSearchtext, searchtext, fetchNameSearchData];
 
-export {useRegion, fetchData, bioState };
+};
+
+export {useEffectFunction};
 
